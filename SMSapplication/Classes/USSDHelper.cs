7867f5319c548abf75dddea8f107629f0b974db0
unknown <Ly Xuan Sang>
2018-07-12 15:39:49 +0700

    first commit

---
+++ SMSapplication/Classes/USSDHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SMSapplication
+{
+    public class USSDHelper
+    {
+        #region Open and Close Ports
+        //Open Port
+        public SerialPort OpenPort(string portName, int baudRate, int dataBits, int readTimeout, int writeTimeout)
+        {
+            receiveNow = new AutoResetEvent(false);
+            SerialPort port = new SerialPort();
+
+            try
+            {
+                port.PortName = portName;                 //COM1
+                port.BaudRate = baudRate;                   //9600
+                port.DataBits = dataBits;                   //8
+                port.StopBits = StopBits.One;                  //1
+                port.Parity = Parity.None;                     //None
+                port.ReadTimeout = readTimeout;             //300
+                port.WriteTimeout = writeTimeout;           //300
+                port.Encoding = Encoding.GetEncoding("iso-8859-1");
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                port.Open();
+                port.DtrEnable = true;
+                port.RtsEnable = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return port;
+        }
+
+        //Close Port
+        public void ClosePort(SerialPort port)
+        {
+            try
+            {
+                port.Close();
+                port.DataReceived -= new SerialDataReceivedEventHandler(port_DataReceived);
+                port = null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }  
+        #endregion
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (e.EventType == SerialData.Chars)
+                {
+                    receiveNow.Set();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ReadResponse(SerialPort port, int timeout)
+        {
+            string serialPortData = string.Empty;
+            try
+            {
+                do
+                {
+                    if (receiveNow.WaitOne(timeout, false))
+                    {
+                        string data = port.ReadExisting();
+                        serialPortData += data;
+                    }
+                    else
+                    {
+                        if (serialPortData.Length > 0)
+                            throw new ApplicationException("Response received is incomplete.");
+                        else
+                            throw new ApplicationException("No data received from phone.");
+                    }
+                }
+                while (!serialPortData.EndsWith("\r\nOK\r\n") && !serialPortData.EndsWith("\r\n> ") && !serialPortData.EndsWith("\r\nERROR\r\n"));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return serialPortData;
+        }
+
+        public string SendATCommand(SerialPort port, string command, int responseTimeout, string errorMessage)
+        {
+            try
+            {
+                port.DiscardOutBuffer();
+                port.DiscardInBuffer();
+                receiveNow.Reset();
+                port.Write(command + "\r");
+
+                string input = ReadResponse(port, responseTimeout);
+                if ((input.Length == 0) || ((!input.EndsWith("\r\n> ")) && (!input.EndsWith("\r\nOK\r\n"))))
+                    throw new ApplicationException("No success message was received.");
+                return input;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }   
+
+
+        public AutoResetEvent receiveNow;
+    }
+}
